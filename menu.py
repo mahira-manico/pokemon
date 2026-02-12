ce4b92a0a1c4193cd245538541be7eaf43110ed6
mahira-manico <mahira.manico@laplateforme.io>
2026-02-12 17:44:46 +0100

    feat: initializing of menu file and constant.py for game constants

---
+++ menu.py
@@ -0,0 +1,12 @@
+import pygame
+from constant import *
+
+
+pygame.init()
+
+screen=pygame.display.set_mode(SCREEN_WIDTH,SCREEN_HEIGHT)
+pygame.display.set_caption("Pokémon")
+font=pygame.font.SysFont("Arial",40)
+
+def main_menu():
+        pass
